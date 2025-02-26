a0a84c44b26036dbb7dca1f3f6218cb270524114
XiaoFeng <meterscao@gmail.com>
2025-02-27 00:02:24 +0800

    Initial Commit

---
+++ TempoPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TempoPro
+//
+//  Created by XiaoFeng on 2025/2/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TempoPro/TempoProApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempoProApp.swift
+//  TempoPro
+//
+//  Created by XiaoFeng on 2025/2/27.
+//
+
+import SwiftUI
+
+@main
+struct TempoProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
